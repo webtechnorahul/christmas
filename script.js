cfd366b5c0348134f07164f495a613430d9b8bdc
webtechnorahul <rahulweb882535@gmail.com>
2025-12-14 13:29:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,14 +12,6 @@ setInterval(()=>{
     }, 1000);
 }); 
 },500)
-// console.log(star);
-// let i=0
-// star.style.color = "red";
-// let triangle1=document.querySelector('.triangle1');
-// let triangle2=document.querySelector('.triangle2');
-// let triangle3=document.querySelector('.triangle3');
-// let triangle4=document.querySelector('.triangle4');
-// let triangle5=document.querySelector('.triangle5');
 
 setInterval(()=>{
     let red=Math.floor(Math.random()*256);
@@ -28,3 +20,33 @@ setInterval(()=>{
     star.style.color=`rgb(${red},${green},${blue})`;
 },300)
 //
+const arr=["giftbox.png","giftbox2.png","giftbox3.png","giftbox4.png","giftbox5.png",
+    "giftbox6.png","giftbox7.png","giftbox8.png","giftbox9.png"
+];
+let box=setInterval(() => {
+   try{
+     let num=Math.floor(Math.random()*arr.length);
+let topp=Math.floor(Math.random()*70)+"%";
+let leftpo=Math.floor(Math.random()*70)+"%";
+let newbox=document.createElement("img");
+newbox.src=`./${arr[num]}`;
+newbox.style.height="100px";
+newbox.style.width="100px";
+newbox.style.position="absolute";
+newbox.style.left=`${leftpo}`;
+newbox.style.top=`${topp}`;
+
+document.body.appendChild(newbox);
+setTimeout(()=>{
+    newbox.remove();
+},3000)
+   }
+   catch(e){
+    console.log(e);
+   }
+
+}, 500);
+
+setTimeout(()=>{
+clearInterval(box);
+},30000)
